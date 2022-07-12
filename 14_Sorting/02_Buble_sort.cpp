f71c2b95e05ece12364c14ba7e0a10acc1fa0ca6
Samar_p <samarpawase@gmail.com>
2022-07-12 22:35:31 +0530

    minor change

--- 14_Sorting/02_Buble_sort.cpp
+++ 14_Sorting/02_Buble_sort.cpp
@@ -17,7 +17,7 @@ int main()
 
     // Sorting Algo
     for (int i = 0; i < n-1;i++){
-        for (int j = 0; j < n - i;j++){
+        for (int j = 0; j < n - i -1;j++){
             if (arr[j]>arr[j+1]){
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
